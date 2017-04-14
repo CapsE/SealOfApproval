f500af93a405cdda3ce22e1d8f801f906e67c30b
Lars <larskrafft@yahoo.de>
2017-04-14 09:59:29 +0200

    Responding with JSON

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.post('/seal', function(req, res){
-   res.send('https://cdn.shopify.com/s/files/1/0860/3518/products/sealofapproval_comp.jpg?v=1432862016');
+    var data = {
+        "text": "Seal of approval",
+        "attachments": [
+            {
+                "thumb_url":"https://cdn.shopify.com/s/files/1/0860/3518/products/sealofapproval_comp.jpg?v=1432862016"
+            }
+        ]
+    };
+   res.send(JSON.stringify(data));
 });
 
 app.listen(process.env.PORT || 3000, function () {
