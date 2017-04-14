75edcb3b9ca6e7dbb5667d1df586342881396c3d
Lars <larskrafft@yahoo.de>
2017-04-14 09:49:12 +0200

    Added seal route

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+app.post('/seal', function(req, res){
+   res.send('https://cdn.shopify.com/s/files/1/0860/3518/products/sealofapproval_comp.jpg?v=1432862016');
+});
+
 app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
 });
